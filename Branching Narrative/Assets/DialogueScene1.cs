d381f62ff75f2ad503ebd4b5f7107646434b19ac
RainyCilla <panjurthe@massart.edu>
2020-10-05 20:51:34 -0400

    Scene1 First draft

---
+++ Branching Narrative/Assets/DialogueScene1.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class DialogueScene1 : MonoBehaviour
+{
+    public int primeInt = 1; // This integer drives game progress!
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    public Text Char3name;
+    public Text Char3speech;
+    public GameObject dialogue;
+    public GameObject ArtChar1;
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    //public GameObject gameHandler;
+    //public AudioSource audioSource;
+    private bool allowSpace = true;
+
+    void Start()
+    {         // initial visibility settings
+        dialogue.SetActive(false);
+        ArtChar1.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    void Update()
+    {         // use spacebar as Next button
+        if (allowSpace == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                talking();
+            }
+        }
+    }
+
+    public void talking()
+    {         // main story function. Players hit next to progress to next int
+        primeInt = primeInt + 1;
+        if (primeInt == 1)
+        {
+            // AudioSource.Play();
+        }
+        else if (primeInt == 2)
+        {
+            
+            dialogue.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Ugh. What a hard day.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 3)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "And tomorrow is goign to be even worse!";
+            Char2name.text = "";
+            Char2speech.text = "";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 4)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "I can't believe Sheila said I was a computer addict.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 5)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "What does she know?";
+            Char2name.text = "";
+            Char2speech.text = "";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 6)
+        {
+            ArtChar1.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "COMPUTER";
+            Char2speech.text = "Boooooop";
+        }
+        else if (primeInt == 7)
+        {
+            ArtChar1.SetActive(false);
+            Char1name.text = "YOU";
+            Char1speech.text = "Ugh. A message. I really shouldn't check that.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 8)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "I really should just go to bed.";
+            Char2name.text = "";
+            Char2speech.text = "";
+            // Turn off "Next" button, turn on "Choice" buttons
+            nextButton.SetActive(false);
+            allowSpace = false;
+            Choice1a.SetActive(true); // function Choice1aFunct()
+            Choice1b.SetActive(true); // function Choice1bFunct()
+        }
+        // ENCOUNTER AFTER CHOICE #1
+        else if (primeInt == 100)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "COMPUTER";
+            Char2speech.text = "Beep boop beep";
+        }
+        else if (primeInt == 101)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "Yeah, yeah, I'm coming!";
+            Char2name.text = "";
+            Char2speech.text = "";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene1Button.SetActive(true);
+        }
+
+        else if (primeInt == 200)
+        {
+            Char1name.text = "COMPUTER";
+            Char1speech.text = "Booooooooooooop";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 201)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "I can't let Sheila be right rght about me! Sleep, here I come!";
+            Char2name.text = "";
+            Char2speech.text = "";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene2Button.SetActive(true);
+        }
+    }
+
+    // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
+    public void Choice1aFunct()
+    {
+        Char1name.text = "YOU";
+        Char1speech.text = "Just checking one email couldn't hurt";
+        Char2name.text = "";
+        Char2speech.text = "";
+        primeInt = 99;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+    public void Choice1bFunct()
+    {
+        Char1name.text = "YOU";
+        Char1speech.text = "I really need to be smart about this.";
+        Char2name.text = "";
+        Char2speech.text = "";
+        primeInt = 199;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+
+    public void SceneChange2a()
+    {
+        SceneManager.LoadScene("Scene2a");
+    }
+    public void SceneChange2b()
+    {
+        SceneManager.LoadScene("Scene2b");
+    }
+}
