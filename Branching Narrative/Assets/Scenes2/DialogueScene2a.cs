e4e417dd87cff558128c8817eddabdb05ab690d5
Skeiron06 <72222721+Skeiron06@users.noreply.github.com>
2020-10-07 13:08:37 -0400

    Scene2A
    Problem in the buttons?

--- Branching Narrative/Assets/Scenes2/DialogueScene2a.cs
+++ Branching Narrative/Assets/Scenes2/DialogueScene2a.cs
@@ -17,8 +17,8 @@ public class DialogueScene2a : MonoBehaviour
     public GameObject dialogue;
     public GameObject ArtChar1;
     public GameObject ArtBG1;
-    public GameObject Choice1a;
-    public GameObject Choice1b;
+    public GameObject Choice3;
+    public GameObject Choice2b;
     public GameObject NextScene1Button;
     public GameObject NextScene2Button;
     public GameObject nextButton;
@@ -31,8 +31,8 @@ public class DialogueScene2a : MonoBehaviour
         dialogue.SetActive(false);
         ArtChar1.SetActive(false);
         ArtBG1.SetActive(true);
-        Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
+        Choice3.SetActive(false);
+        Choice2b.SetActive(false);
         NextScene1Button.SetActive(false);
         NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
@@ -111,14 +111,14 @@ public class DialogueScene2a : MonoBehaviour
             // Turn off "Next" button, turn on "Choice" buttons
             nextButton.SetActive(false);
             allowSpace = false;
-            Choice1a.SetActive(true); // function Choice1aFunct()
-            Choice1b.SetActive(true); // function Choice1bFunct()
+            Choice3.SetActive(true); // function Choice1aFunct()
+            Choice2b.SetActive(true); // function Choice1bFunct()
         }
         // ENCOUNTER AFTER CHOICE #1
         else if (primeInt == 100)
         {
             Char1name.text = "You";
-            Char1speech.text = "I guess, I can check the computer.";
+            Char1speech.text = "Well… I should just get in bed then.";
             Char2name.text = "";
             Char2speech.text = "";
         }
@@ -126,39 +126,39 @@ public class DialogueScene2a : MonoBehaviour
         else if (primeInt == 200)
         {
             Char1name.text = "You";
-            Char1speech.text = "Well… I should just get in bed then.";
+            Char1speech.text = "I guess, I can check the computer.";
             Char2name.text = "";
             Char2speech.text = "";
         }
     }
 
     // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
-    public void Choice2bFunct()
-    {
-        Char1name.text = "";
-        Char1speech.text = "";
-        Char2name.text = "";
-        Char2speech.text = "";
-        primeInt = 99;
-        Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
-        nextButton.SetActive(true);
-        allowSpace = true;
-    }
     public void Choice3Funct()
     {
         Char1name.text = "";
         Char1speech.text = "";
         Char2name.text = "";
         Char2speech.text = "";
+        primeInt = 99;
+        Choice3.SetActive(false);
+        Choice2b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+    public void Choice2bFunct()
+    {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
         primeInt = 199;
-        Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
+        Choice3.SetActive(false);
+        Choice2b.SetActive(false);
         nextButton.SetActive(true);
         allowSpace = true;
     }
 
-    public void SceneChange2a()
+    public void SceneChange3()
     {
         SceneManager.LoadScene("Scene2a");
     }
