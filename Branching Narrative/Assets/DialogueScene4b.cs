6da3c448470793c5f328c59a33a433d8d06b7343
gqinsky <72154174+gqinsky@users.noreply.github.com>
2020-10-07 16:11:25 -0400

    4b finished

--- Branching Narrative/Assets/DialogueScene4b.cs
+++ Branching Narrative/Assets/DialogueScene4b.cs
@@ -18,7 +18,6 @@ public class DialogueScene4b : MonoBehaviour
     public GameObject ArtChar1;
     public GameObject ArtBG1;
     public GameObject Choice1a;
-    public GameObject Choice1b;
     public GameObject NextScene1Button;
     public GameObject NextScene2Button;
     public GameObject nextButton;
@@ -32,7 +31,6 @@ public class DialogueScene4b : MonoBehaviour
         ArtChar1.SetActive(false);
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
         NextScene1Button.SetActive(false);
         NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
@@ -58,7 +56,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 2)
         {
-            ArtChar1.SetActive(true);
+            ArtChar1.SetActive(false);
             dialogue.SetActive(true);
             Char1name.text = "You";
             Char1speech.text = "Let's see...";
@@ -67,6 +65,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 3)
         {
+            ArtChar1.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "Phone";
@@ -75,6 +74,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 4)
         {
+            ArtChar1.SetActive(false);
             Char1name.text = "You";
             Char1speech.text = "Yes?";
             Char2name.text = "";
@@ -82,6 +82,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 5)
         {
+            ArtChar1.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "Phone";
@@ -90,6 +91,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 6)
         {
+            ArtChar1.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "Phone";
@@ -97,6 +99,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 7)
         {
+            ArtChar1.SetActive(false);
             Char1name.text = "You";
             Char1speech.text = "Hmm...";
             Char2name.text = "";
@@ -104,6 +107,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 8)
         {
+            ArtChar1.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "Phone";
@@ -111,6 +115,7 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 9)
         {
+            ArtChar1.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "Phone";
@@ -125,34 +130,29 @@ public class DialogueScene4b : MonoBehaviour
         }
         else if (primeInt == 11)
         {
+            ArtChar1.SetActive(false);
             Char1name.text = "You";
             Char1speech.text = "What's taking him so long?";
             Char2name.text = "";
             Char2speech.text = "";
             // Turn off "Next" button, turn on "Choice" buttons
-            nextButton.SetActive(true);
+            nextButton.SetActive(false);
             allowSpace = false;
-            Choice1a.SetActive(false); // function Choice1aFunct()
-            Choice1b.SetActive(false); // function Choice1bFunct()
+            Choice1a.SetActive(true); // function Choice1aFunct()          
         }
         // ENCOUNTER AFTER CHOICE #1
         else if (primeInt == 100)
         {
-            Char1name.text = "Jeda";
-            Char1speech.text = "Then you are no use to me, and must be silenced.";
-            Char2name.text = "";
-            Char2speech.text = "";
-        }
-        else if (primeInt == 101)
-        {
-            Char1name.text = "Jeda";
-            Char1speech.text = "Come back here! Do not think you can hide from me!";
-            Char2name.text = "";
-            Char2speech.text = "";
+            ArtChar1.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Phone";
+            Char2speech.text = "...";
             nextButton.SetActive(false);
             allowSpace = false;
             NextScene1Button.SetActive(true);
         }
+        
 
         else if (primeInt == 200)
         {
@@ -176,13 +176,12 @@ public class DialogueScene4b : MonoBehaviour
     // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
     public void Choice1aFunct()
     {
-        Char1name.text = "";
-        Char1speech.text = "";
-        Char2name.text = "You";
-        Char2speech.text = "I don't know what you're talking about!";
+        Char1name.text = "You";
+        Char1speech.text = "Hello???";
+        Char2name.text = "";
+        Char2speech.text = "";
         primeInt = 99;
         Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
         nextButton.SetActive(true);
         allowSpace = true;
     }
@@ -194,17 +193,13 @@ public class DialogueScene4b : MonoBehaviour
         Char2speech.text = "Sure, anything you want... just lay off the club.";
         primeInt = 199;
         Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
-        nextButton.SetActive(true);
-        allowSpace = true;
+        nextButton.SetActive(false);
+        allowSpace = false;
     }
 
-    public void SceneChange5a()
+    public void SceneChange5()
     {
-        SceneManager.LoadScene("Scene5a");
+        SceneManager.LoadScene("Scene5");
     }
-    public void SceneChange5b()
-    {
-        SceneManager.LoadScene("Scene5b");
-    }
+    
 }
