bcf0a3e453b314a51fcabfaa019d439aae23a24f
Whitel3ss <achen@massart.edu>
2020-10-21 23:14:48 -0400

    TypeWriter

---
+++ Branching Narrative/Assets/Scripts/TypeWriter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TypeFX : MonoBehaviour
+{
+
+    public float delay = 0.01f;
+    public string fullText;
+    private string currentText = "";
+
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < (fullText.Length + 1); i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
